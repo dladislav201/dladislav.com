eeeb2f2c4b8df1ed10b9e76ff4a2ab050e6e438d
dladislav201 <dobrodii.vlad201@gmail.com>
2025-04-21 20:46:59 +0200

    Implement initial project dependencies

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,16 @@
 import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
-
-dotenv.config();
+import helmet from 'helmet';
+import morgan from 'morgan';
+import compression from 'compression';
+import { config } from './config/env';
 
 const app: Express = express();
-const PORT = process.env.PORT || 3001;
 
+app.use(helmet());
 app.use(cors());
+app.use(compression());
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -24,6 +27,6 @@ app.use((err: Error, req: Request, res: Response, next: Function) => {
   res.status(500).json({ error: 'Something went wrong!' });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(config.port, () => {
+  console.log(`Server running on port ${config.port}`);
 });
